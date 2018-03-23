67e5f1e3a0ccdbff71dec487a3690aa7567d29ef
Jelly <lidongking@126.com>
2018-03-23 16:36:49 +0800

    CORS set.

--- js.php
+++ js.php
@@ -9,7 +9,7 @@
 header('Content-Type:text/html;charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
-header('Access-Control-Request-Method: GET,POST,OPTION');
+header('Access-Control-Request-Method: GET,POST,OPTIONS');
 $method = ($_SERVER['REQUEST_METHOD']);
 if ($method == 'POST')
 {
